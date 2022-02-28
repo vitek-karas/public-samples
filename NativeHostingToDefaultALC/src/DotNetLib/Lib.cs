b341b74e5c7487e1c761dc492e0cf851f5852df6
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2022-02-28 07:07:06 -0800

    Add a sample for loading managed code into existing managed process and to get to default ALC

---
+++ NativeHostingToDefaultALC/src/DotNetLib/Lib.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+
+namespace DotNetLib
+{
+    public static class Lib
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LibArgs
+        {
+            public IntPtr Message;
+            public int Number;
+        }
+
+        [UnmanagedCallersOnly]
+        public static void CustomEntryPointUnmanaged(LibArgs libArgs)
+        {
+            Console.WriteLine($"Hello, world! from {nameof(CustomEntryPointUnmanaged)} in {nameof(Lib)}");
+            PrintLibArgs(libArgs);
+
+            Assembly asm = Assembly.GetExecutingAssembly();
+            Console.WriteLine($"Running in {asm.GetName().Name} in ALC = {AssemblyLoadContext.GetLoadContext(asm)}");
+
+            // Load this assembly again, but this time to default ALC
+            Assembly asmInDefault = AssemblyLoadContext.Default.LoadFromAssemblyPath(asm.Location);
+            // Find the type again
+            Type typeInDefault = asmInDefault.GetType(typeof(Lib).FullName);
+            // Find the EntryPointToDefaultALC method
+            MethodInfo methodInDefault = typeInDefault.GetMethod(nameof(EntryPointToDefaultALC));
+            methodInDefault.Invoke(null, new object[] { });
+        }
+
+        public static void EntryPointToDefaultALC()
+        {
+            Console.WriteLine($"Hello from default ALC!");
+
+            Assembly asm = Assembly.GetExecutingAssembly();
+            Console.WriteLine($"Running in {asm.GetName().Name} in ALC = {AssemblyLoadContext.GetLoadContext(asm)}");
+        }
+
+        private static void PrintLibArgs(LibArgs libArgs)
+        {
+            string message = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? Marshal.PtrToStringUni(libArgs.Message)
+                : Marshal.PtrToStringUTF8(libArgs.Message);
+
+            Console.WriteLine($"-- message: {message}");
+            Console.WriteLine($"-- number: {libArgs.Number}");
+        }
+    }
+}
---
+++ NativeHostingToDefaultALC/src/HostApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Calling to native");
+InjectManaged();
+
+[DllImport("nativehost.dll", EntryPoint = "?InjectManaged@@YAHXZ")]
+static extern void InjectManaged();
